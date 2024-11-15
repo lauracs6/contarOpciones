dd83f1dd1e4df189ebdcfba1503a1ba0b6c28268
Laura Cabrera Sagrera <lauracabrerasagrera@gmail.com>
2024-11-15 09:15:52 +0100

    Create contarOpciones.java

---
+++ contarOpciones.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] arrayOpciones = {
+                {1, 2},
+                {4},
+                {5, 6}
+        };
+
+        System.out.println("La cantidad de matrices únicas que se pueden formar es: " + contarCombinacionesUnicas(arrayOpciones));
+    }
+
+    public static int contarCombinacionesUnicas(int[][] arrays) {
+        int cantidadTotal = 0;
+
+        for (int i = 0; i < arrays.length; i++) {
+            int[] subarray = arrays[i]; //
+            int cont_opciones = 0;
+            int[] arrayOpciones = new int[subarray.length];
+            int opcionesSize = 0;
+
+            for (int j = 0; j < subarray.length; j++) {
+                int elemento = subarray[j]; // 
+                boolean existe = false;
+
+                for (int k = 0; k < opcionesSize; k++) {
+                    if (arrayOpciones[k] == elemento) { //
+                        existe = true;
+                        break;
+                    }
+                }
+
+                if (!existe) {
+                    arrayOpciones[opcionesSize++] = elemento;
+                    cont_opciones++;
+                }
+            }
+
+            cantidadTotal += cont_opciones;
+        }
+        return cantidadTotal;
+    }
+}
